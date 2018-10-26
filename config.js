2fbd333b8da0d950dda97d51b4e3ec80b40cdff6
sanjeev <kksanjeevn7215@gmail.com>
2018-10-27 00:28:16 +0530

    ffds

---
+++ config.js
@@ -0,0 +1,14 @@
+'use strict'
+
+module.exports = {
+	mailer: {
+		service: 'gmail',
+		auth: {
+			user: 'kksanjeevn7215@gmail.com',
+			pass: 'cashshow'
+			}
+		},
+	 dbConnstring: 'mongodb://admin:cashshow123@ds137703.mlab.com:37703/webshare',
+	// dbConnstring: 'mongodb://127.0.0.1:27017/WebShare',
+	sessionKey: 'HaloWebshare'
+}
